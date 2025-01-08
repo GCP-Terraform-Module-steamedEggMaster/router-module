7045449a908c34f8c3f5da3f647132dd3e59b4a5
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-09 04:02:47 +0900

    test : add output values

--- examples/with-vpc/outputs.tf
+++ examples/with-vpc/outputs.tf
@@ -7,3 +7,23 @@ output "router_id" {
   value = module.router.router_id
   description = "The network ID from the VPC module"
 }
+
+output "router_name" {
+  description = "생성된 라우터의 이름"
+  value       = module.router.name
+}
+
+output "router_self_link" {
+  description = "생성된 라우터의 self-link"
+  value       = module.router.self_link
+}
+
+output "router_region" {
+  description = "생성된 라우터가 위치한 지역"
+  value       = module.router.region
+}
+
+output "router_network" {
+  description = "생성된 라우터가 속한 네트워크"
+  value       = module.router.network
+}
