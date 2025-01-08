a4fec64921e8c14f65dbf5ecf642b67084b4ef18
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-09 07:31:01 +0900

    fix : apply terraform fmt

--- examples/with-vpc/main.tf
+++ examples/with-vpc/main.tf
@@ -1,13 +1,13 @@
 module "vpc" {
-  source  = "git::https://github.com/GCP-Terraform-Module-steamedEggMaster/vpc-module.git?ref=v1.0.0"
+  source = "git::https://github.com/GCP-Terraform-Module-steamedEggMaster/vpc-module.git?ref=v1.0.0"
 
   vpc_network_name = var.vpc_network_name
 }
 
 module "router" {
-    source = "../../"
-    
-    router_name    = var.router_name
-    router_region  = var.router_region
-    network_id     = module.vpc.network_id
+  source = "../../"
+
+  router_name   = var.router_name
+  router_region = var.router_region
+  network_id    = module.vpc.network_id
 }
--- examples/with-vpc/outputs.tf
+++ examples/with-vpc/outputs.tf
@@ -1,10 +1,10 @@
 output "network_id" {
-  value = module.vpc.network_id
+  value       = module.vpc.network_id
   description = "The network ID from the VPC module"
 }
 
 output "router_id" {
-  value = module.router.router_id
+  value       = module.router.router_id
   description = "The network ID from the VPC module"
 }
 
