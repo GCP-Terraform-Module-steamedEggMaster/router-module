d54e49a71a75eb97c8598c238b77734495a02417
steamedEggMaster <149857288+steamedEggMaster@users.noreply.github.com>
2025-01-10 17:36:38 +0900

    fix : modify vpc module name

--- examples/with-vpc/main.tf
+++ examples/with-vpc/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "git::https://github.com/GCP-Terraform-Module-steamedEggMaster/vpc-module.git?ref=v1.0.0"
 
-  vpc_network_name = "test-vpc"
+  name = "test-vpc"
 }
 
 module "router" {
